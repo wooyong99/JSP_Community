34c4b172ed08b800edb8b9b992b7b948778ab0f0
wooyong99 <wulovesk@naver.com>
2023-01-14 10:55:44 +0900

    서블릿으로 구구단 구현

---
+++ src/main/java/GugudanServlet.java
@@ -0,0 +1,25 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.setCharacterEncoding("UTF-8");
+    resp.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html; charset-utf-8");
+
+    int dan = Integer.parseInt(req.getParameter("dan"));
+    int limit = Integer.parseInt(req.getParameter("limit"));
+    resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+
+    for(int i=1; i <= limit; i++){
+      resp.getWriter().append("<div>%d * %d  = %d</div>".formatted(dan, i, dan*i));
+    }
+
+  }
+}
--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
     resp.setCharacterEncoding("UTF-8"); // 응답하는 데이터를 UTF-8로 지정
     resp.setContentType("text/html; charset-utf-8");  // 브라우저에 노출되는 결과물을 UTF-8로 지정
 
-    resp.getWriter().append("안녕하세요.");
+    resp.getWriter().append("안녕하세요.1");
 
   }
 }
