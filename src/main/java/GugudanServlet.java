7a8efa47f198d8abe073ef7238b009f7243317f7
wooyong99 <wulovesk@naver.com>
2023-01-14 21:56:50 +0900

    Rq 클래스 도입

--- src/main/java/GugudanServlet.java
+++ src/main/java/GugudanServlet.java
@@ -9,16 +9,14 @@ import java.io.IOException;
 public class GugudanServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.setCharacterEncoding("UTF-8");
-    resp.setCharacterEncoding("UTF-8");
-    resp.setContentType("text/html; charset-utf-8");
+    Rq rq = new Rq(req, resp);
 
-    int dan = Integer.parseInt(req.getParameter("dan"));
-    int limit = Integer.parseInt(req.getParameter("limit"));
-    resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+    int dan = rq.getIntParam("dan", 0);
+    int limit = rq.getIntParam("limit", 0);
+    rq.appendBody("<h1>%d단</h1>".formatted(dan));
 
     for(int i=1; i <= limit; i++){
-      resp.getWriter().append("<div>%d * %d  = %d</div>".formatted(dan, i, dan*i));
+      rq.appendBody("<div>%d * %d  = %d</div>".formatted(dan, i, dan*i));
     }
 
   }
---
+++ src/main/java/Rq.java
@@ -0,0 +1,45 @@
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class Rq {
+  private final HttpServletRequest req;
+  private final HttpServletResponse resp;
+
+  public Rq(HttpServletRequest req, HttpServletResponse resp) {
+    this.req = req;
+    this.resp = resp;
+
+    try {
+      req.setCharacterEncoding("UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+    resp.setCharacterEncoding("UTF-8");
+    resp.setContentType("text/html; charset-utf-8");
+  }
+
+  public int getIntParam(String paramName, int defaultValue){
+    String paramValue = req.getParameter(paramName);
+
+    if(paramValue == null){
+      return defaultValue;
+    }else{
+      try{
+        return Integer.parseInt(paramValue);
+      }catch(NumberFormatException e){
+        return defaultValue;
+      }
+    }
+  }
+
+  public void appendBody(String str){
+    try {
+      resp.getWriter().append(str);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
