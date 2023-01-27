e31dfbab938eb7731127e8d7cd6b94473ecacdb2
wooyong99 <wulovesk@naver.com>
2023-01-27 13:30:19 +0900

    불필요 파일 제거 및 메인페이지 생성

--- src/main/java/com/jwy/exam/servlet/HomeMainServlet.java
+++ src/main/java/com/jwy/exam/servlet/HomeMainServlet.java
@@ -8,15 +8,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/home/main")
+public class HomeMainServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.setCharacterEncoding("UTF-8");  // 요청되는 데이터를 UTF-8로 지정
-    resp.setCharacterEncoding("UTF-8"); // 응답하는 데이터를 UTF-8로 지정
-    resp.setContentType("text/html; charset-utf-8");  // 브라우저에 노출되는 결과물을 UTF-8로 지정
-
-    resp.getWriter().append("안녕하세요.1");
-
+    req.getRequestDispatcher("../home/main.jsp").forward(req,resp);
   }
 }
